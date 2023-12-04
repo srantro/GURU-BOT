28937726cb1c0a32d6ac9094331214fae716d080
srantro <152559582+srantro@users.noreply.github.com>
2023-12-04 16:33:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['254103502391', '6wzdbr', true],
-  ['6281255369012', 'Ash', true], 
+  ['917605902011', 'GURU', true], 
   [''] 
 ] //Number of owners
 
